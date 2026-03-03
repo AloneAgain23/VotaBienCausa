bb836e609041e9dba1bb06113d6a2dde95682230
AloneAgain23 <112266122+AloneAgain23@users.noreply.github.com>
2026-03-03 13:05:15 -0500

    Update candidatos.ts

--- src/lib/candidatos.ts
+++ src/lib/candidatos.ts
@@ -14,7 +14,7 @@ export const CANDIDATOS: Candidato[] = [
     nombre: "PABLO ALFONSO LOPEZ CHAU NAVA",
     partido: "AHORA NACION - AN",
     foto_url: "https://mpesije.jne.gob.pe/apidocs/ddfa74eb-cae3-401c-a34c-35543ae83c57.jpg",
-    perfil: null,
+    perfil: "Tecnócrata",
     P1: 0, P2: 0, P3: 0, P4: 5, P5: 5,
     P6: 2, P7: 0, P8: 2, P9: 2, P10: 4,
     P11: 5, P12: 4, P13: 3, P14: 3, P15: 3
@@ -24,7 +24,7 @@ export const CANDIDATOS: Candidato[] = [
     nombre: "RONALD DARWIN ATENCIO SOTOMAYOR",
     partido: "ALIANZA ELECTORAL VENCEREMOS",
     foto_url: "https://mpesije.jne.gob.pe/apidocs/bac0288d-3b21-45ac-8849-39f9177fb020.jpg",
-    perfil: null,
+    perfil: "Outsider",
     P1: 1, P2: 2, P3: 4, P4: 5, P5: 4,
     P6: 4, P7: 4, P8: 4, P9: 1, P10: 2,
     P11: 1, P12: 3, P13: 4, P14: 2, P15: 4
@@ -34,7 +34,7 @@ export const CANDIDATOS: Candidato[] = [
     nombre: "CESAR ACUÑA PERALTA",
     partido: "ALIANZA PARA EL PROGRESO",
     foto_url: "https://mpesije.jne.gob.pe/apidocs/d6fe3cac-7061-474b-8551-0aa686a54bad.jpg",
-    perfil: null,
+    perfil: "Político con experiencia",
     P1: 0, P2: 4, P3: 4, P4: 4, P5: 5,
     P6: 1, P7: 4, P8: 2, P9: 4, P10: 4,
     P11: 5, P12: 4, P13: 5, P14: 4, P15: 5
@@ -44,7 +44,7 @@ export const CANDIDATOS: Candidato[] = [
     nombre: "JOSE DANIEL WILLIAMS ZAPATA",
     partido: "AVANZA PAIS - PARTIDO DE INTEGRACION SOCIAL",
     foto_url: "https://mpesije.jne.gob.pe/apidocs/b60c471f-a6bb-4b42-a4b2-02ea38acbb0d.jpg",
-    perfil: null,
+    perfil: "Político con experiencia",
     P1: 5, P2: 2, P3: 0, P4: 4, P5: 5,
     P6: 1, P7: 0, P8: 2, P9: 4, P10: 4,
     P11: 4, P12: 4, P13: 3, P14: 5, P15: 4
@@ -54,7 +54,7 @@ export const CANDIDATOS: Candidato[] = [
     nombre: "ALVARO GONZALO PAZ DE LA BARRA FREIGEIRO",
     partido: "FE EN EL PERU",
     foto_url: "https://votoinformado.jne.gob.pe/assets/images/candidatos/ALVARO%20PAZ%20DE%20LA%20BARRA.jpg",
-    perfil: null,
+    perfil: "Político con experiencia",
     P1: 1, P2: 5, P3: 0, P4: 0, P5: 0,
     P6: 0, P7: 0, P8: 0, P9: 3, P10: 3,
     P11: 0, P12: 4, P13: 0, P14: 0, P15: 0
@@ -64,7 +64,7 @@ export const CANDIDATOS: Candidato[] = [
     nombre: "KEIKO SOFIA FUJIMORI HIGUCHI",
     partido: "FUERZA POPULAR",
     foto_url: "https://mpesije.jne.gob.pe/apidocs/251cd1c0-acc7-4338-bd8a-439ccb9238d0.jpeg",
-    perfil: null,
+    perfil: "Político con experiencia",
     P1: 2, P2: 4, P3: 4, P4: 4, P5: 4,
     P6: 3, P7: 0, P8: 1, P9: 3, P10: 4,
     P11: 5, P12: 4, P13: 5, P14: 5, P15: 5
@@ -74,7 +74,7 @@ export const CANDIDATOS: Candidato[] = [
     nombre: "FIORELLA GIANNINA MOLINELLI ARISTONDO",
     partido: "FUERZA Y LIBERTAD",
     foto_url: "https://mpesije.jne.gob.pe/apidocs/1de656b5-7593-4c60-ab7a-83d618a3d80d.jpg",
-    perfil: null,
+    perfil: "Tecnócrata",
     P1: 2, P2: 2, P3: 5, P4: 5, P5: 5,
     P6: 2, P7: 2, P8: 2, P9: 2, P10: 3,
     P11: 1, P12: 4, P13: 5, P14: 1, P15: 0
@@ -84,7 +84,7 @@ export const CANDIDATOS: Candidato[] = [
     nombre: "ROBERTO HELBERT SANCHEZ PALOMINO",
     partido: "JUNTOS POR EL PERU",
     foto_url: "https://mpesije.jne.gob.pe/apidocs/bb7c7465-9c6e-44eb-ac7d-e6cc7f872a1a.jpg",
-    perfil: null,
+    perfil: "Político con experiencia",
     P1: 0, P2: 0, P3: 0, P4: 5, P5: 4,
     P6: 4, P7: 4, P8: 5, P9: 1, P10: 1,
     P11: 1, P12: 4, P13: 5, P14: 4, P15: 4
@@ -94,7 +94,7 @@ export const CANDIDATOS: Candidato[] = [
     nombre: "RAFAEL JORGE BELAUNDE LLOSA",
     partido: "LIBERTAD POPULAR",
     foto_url: "https://mpesije.jne.gob.pe/apidocs/3302e45b-55c8-4979-a60b-2b11097abf1d.jpg",
-    perfil: null,
+    perfil: "Tecnócrata",
     P1: 1, P2: 1, P3: 0, P4: 5, P5: 5,
     P6: 1, P7: 0, P8: 1, P9: 3, P10: 5,
     P11: 4, P12: 0, P13: 1, P14: 0, P15: 0
@@ -104,7 +104,7 @@ export const CANDIDATOS: Candidato[] = [
     nombre: "PITTER ENRIQUE VALDERRAMA PEÑA",
     partido: "PARTIDO APRISTA PERUANO",
     foto_url: "https://mpesije.jne.gob.pe/apidocs/d72c4b29-e173-42b8-b40d-bdb6d01a526a.jpg",
-    perfil: null,
+    perfil: "Político con experiencia",
     P1: 0, P2: 4, P3: 4, P4: 5, P5: 5,
     P6: 3, P7: 4, P8: 4, P9: 3, P10: 3,
     P11: 0, P12: 4, P13: 2, P14: 4, P15: 0
@@ -114,7 +114,7 @@ export const CANDIDATOS: Candidato[] = [
     nombre: "RICARDO PABLO BELMONT CASSINELLI",
     partido: "PARTIDO CIVICO OBRAS",
     foto_url: "https://mpesije.jne.gob.pe/apidocs/78647f15-d5d1-4ed6-8ac6-d599e83eeea3.jpg",
-    perfil: null,
+    perfil: "Político con experiencia",
     P1: 0, P2: 0, P3: 0, P4: 4, P5: 0,
     P6: 0, P7: 0, P8: 0, P9: 2, P10: 2,
     P11: 0, P12: 0, P13: 0, P14: 0, P15: 0
@@ -124,7 +124,7 @@ export const CANDIDATOS: Candidato[] = [
     nombre: "NAPOLEON BECERRA GARCIA",
     partido: "PARTIDO DE LOS TRABAJADORES Y EMPRENDEDORES PTE - PERU",
     foto_url: "https://mpesije.jne.gob.pe/apidocs/bab206cb-b2d5-41ec-bde8-ef8cf3e0a2df.jpg",
-    perfil: null,
+    perfil: "Empresario",
     P1: 0, P2: 4, P3: 0, P4: 0, P5: 0,
     P6: 0, P7: 0, P8: 0, P9: 2, P10: 0,
     P11: 0, P12: 0, P13: 0, P14: 0, P15: 0
@@ -134,7 +134,7 @@ export const CANDIDATOS: Candidato[] = [
     nombre: "JORGE NIETO MONTESINOS",
     partido: "PARTIDO DEL BUEN GOBIERNO",
     foto_url: "https://mpesije.jne.gob.pe/apidocs/9ae56ed5-3d0f-49ff-8bb9-0390bad71816.jpg",
-    perfil: null,
+    perfil: "Político con experiencia",
     P1: 0, P2: 0, P3: 0, P4: 5, P5: 4,
     P6: 0, P7: 4, P8: 4, P9: 2, P10: 3,
     P11: 0, P12: 4, P13: 2, P14: 0, P15: 0
@@ -144,7 +144,7 @@ export const CANDIDATOS: Candidato[] = [
     nombre: "CHARLIE CARRASCO SALAZAR",
     partido: "PARTIDO DEMOCRATA UNIDO PERU",
     foto_url: "https://mpesije.jne.gob.pe/apidocs/12fa17db-f28f-4330-9123-88549539b538.jpg",
-    perfil: null,
+    perfil: "Tecnócrata",
     P1: 4, P2: 4, P3: 5, P4: 4, P5: 4,
     P6: 0, P7: 0, P8: 2, P9: 3, P10: 5,
     P11: 2, P12: 0, P13: 0, P14: 3, P15: 0
@@ -154,7 +154,7 @@ export const CANDIDATOS: Candidato[] = [
     nombre: "ALEX GONZALES CASTILLO",
     partido: "PARTIDO DEMOCRATA VERDE",
     foto_url: "https://mpesije.jne.gob.pe/apidocs/c0ae56bf-21c1-4810-890a-b25c8465bdd9.jpg",
-    perfil: null,
+    perfil: "Político con experiencia",
     P1: 4, P2: 4, P3: 4, P4: 5, P5: 4,
     P6: 0, P7: 0, P8: 2, P9: 3, P10: 3,
     P11: 4, P12: 0, P13: 0, P14: 0, P15: 0
@@ -164,7 +164,7 @@ export const CANDIDATOS: Candidato[] = [
     nombre: "ARMANDO JOAQUIN MASSE FERNANDEZ",
     partido: "PARTIDO DEMOCRATICO FEDERAL",
     foto_url: "https://mpesije.jne.gob.pe/apidocs/cb1adeb7-7d2f-430c-ae87-519137d8edfa.jpg",
-    perfil: null,
+    perfil: "Tecnócrata",
     P1: 0, P2: 4, P3: 0, P4: 4, P5: 4,
     P6: 0, P7: 0, P8: 4, P9: 2, P10: 4,
     P11: 0, P12: 4, P13: 0, P14: 2, P15: 0
@@ -174,7 +174,7 @@ export const CANDIDATOS: Candidato[] = [
     nombre: "GEORGE PATRICK FORSYTH SOMMER",
     partido: "PARTIDO DEMOCRATICO SOMOS PERU",
     foto_url: "https://mpesije.jne.gob.pe/apidocs/b1d60238-c797-4cba-936e-f13de6a34cc7.jpg",
-    perfil: null,
+    perfil: "Político con experiencia",
     P1: 2, P2: 3, P3: 5, P4: 5, P5: 4,
     P6: 0, P7: 0, P8: 2, P9: 2, P10: 4,
     P11: 4, P12: 4, P13: 5, P14: 0, P15: 4
@@ -184,7 +184,7 @@ export const CANDIDATOS: Candidato[] = [
     nombre: "LUIS FERNANDO OLIVERA VEGA",
     partido: "PARTIDO FRENTE DE LA ESPERANZA 2021",
     foto_url: "https://mpesije.jne.gob.pe/apidocs/3e2312e1-af79-4954-abfa-a36669c1a9e9.jpg",
-    perfil: null,
+    perfil: "Político con experiencia" ",
     P1: 2, P2: 4, P3: 4, P4: 5, P5: 5,
     P6: 0, P7: 0, P8: 2, P9: 2, P10: 4,
     P11: 0, P12: 0, P13: 0, P14: 0, P15: 0
@@ -194,7 +194,7 @@ export const CANDIDATOS: Candidato[] = [
     nombre: "MESIAS ANTONIO GUEVARA AMASIFUEN",
     partido: "PARTIDO MORADO",
     foto_url: "https://mpesije.jne.gob.pe/apidocs/1b861ca7-3a5e-48b4-9024-08a92371e33b.jpg",
-    perfil: null,
+    perfil: "Político con experiencia",
     P1: 0, P2: 4, P3: 0, P4: 5, P5: 4,
     P6: 0, P7: 0, P8: 4, P9: 2, P10: 3,
     P11: 1, P12: 3, P13: 1, P14: 2, P15: 2
@@ -204,7 +204,7 @@ export const CANDIDATOS: Candidato[] = [
     nombre: "CARLOS GONSALO ALVAREZ LOAYZA",
     partido: "PARTIDO PAIS PARA TODOS",
     foto_url: "https://mpesije.jne.gob.pe/apidocs/2bd18177-d665-413d-9694-747d729d3e39.jpg",
-    perfil: null,
+    perfil: "Outsider",
     P1: 5, P2: 0, P3: 4, P4: 5, P5: 5,
     P6: 3, P7: 0, P8: 3, P9: 4, P10: 2,
     P11: 0, P12: 4, P13: 0, P14: 4, P15: 3
@@ -214,7 +214,7 @@ export const CANDIDATOS: Candidato[] = [
     nombre: "HERBERT CALLER GUTIERREZ",
     partido: "PARTIDO PATRIOTICO DEL PERU",
     foto_url: "https://mpesije.jne.gob.pe/apidocs/6ad6c5ff-0411-4ddd-9cf7-b0623f373fcf.jpg",
-    perfil: null,
+    perfil: "Outsider",
     P1: 5, P2: 5, P3: 3, P4: 0, P5: 5,
     P6: 2, P7: 0, P8: 0, P9: 2, P10: 4,
     P11: 0, P12: 3, P13: 1, P14: 3, P15: 3
@@ -224,7 +224,7 @@ export const CANDIDATOS: Candidato[] = [
     nombre: "YONHY LESCANO ANCIETA",
     partido: "PARTIDO POLITICO COOPERACION POPULAR",
     foto_url: "https://mpesije.jne.gob.pe/apidocs/b9db2b5c-02ff-4265-ae51-db9b1001ad70.jpg",
-    perfil: null,
+    perfil: "Político con experiencia",
     P1: 5, P2: 4, P3: 4, P4: 0, P5: 5,
     P6: 4, P7: 4, P8: 4, P9: 1, P10: 2,
     P11: 0, P12: 4, P13: 3, P14: 3, P15: 3
@@ -234,7 +234,7 @@ export const CANDIDATOS: Candidato[] = [
     nombre: "WOLFGANG MARIO GROZO COSTA",
     partido: "PARTIDO POLITICO INTEGRIDAD DEMOCRATICA",
     foto_url: "https://mpesije.jne.gob.pe/apidocs/064360d1-ce49-4abe-939c-f4de8b0130a2.jpg",
-    perfil: null,
+    perfil: "Tecnócrata",
     P1: 1, P2: 1, P3: 5, P4: 4, P5: 4,
     P6: 1, P7: 0, P8: 4, P9: 2, P10: 4,
     P11: 3, P12: 3, P13: 1, P14: 3, P15: 3
@@ -244,7 +244,7 @@ export const CANDIDATOS: Candidato[] = [
     nombre: "VLADIMIR ROY CERRON ROJAS",
     partido: "PARTIDO POLITICO NACIONAL PERU LIBRE",
     foto_url: "https://mpesije.jne.gob.pe/apidocs/82ee0ff2-2336-4aba-9590-e576f7564315.jpg",
-    perfil: null,
+    perfil: "Político con experiencia",
     P1: 5, P2: 0, P3: 0, P4: 0, P5: 5,
     P6: 4, P7: 4, P8: 4, P9: 1, P10: 1,
     P11: 5, P12: 5, P13: 4, P14: 4, P15: 4
@@ -254,7 +254,7 @@ export const CANDIDATOS: Candidato[] = [
     nombre: "FRANCISCO ERNESTO DIEZ-CANSECO TÁVARA",
     partido: "PARTIDO POLITICO PERU ACCION",
     foto_url: "https://mpesije.jne.gob.pe/apidocs/2d1bf7f2-6e88-4ea9-8ed2-975c1ae5fb92.jpg",
-    perfil: null,
+    perfil: "Político con experiencia",
     P1: 5, P2: 3, P3: 4, P4: 4, P5: 4,
     P6: 1, P7: 2, P8: 1, P9: 3, P10: 4,
     P11: 0, P12: 3, P13: 1, P14: 3, P15: 3
@@ -264,7 +264,7 @@ export const CANDIDATOS: Candidato[] = [
     nombre: "MARIO ENRIQUE VIZCARRA CORNEJO",
     partido: "PARTIDO POLITICO PERU PRIMERO",
     foto_url: "https://mpesije.jne.gob.pe/apidocs/ee7a080e-bc81-4c81-9e5e-9fd95ff459ab.jpg",
-    perfil: null,
+    perfil: "Empresario",
     P1: 0, P2: 4, P3: 0, P4: 5, P5: 5,
     P6: 2, P7: 0, P8: 0, P9: 3, P10: 4,
     P11: 0, P12: 4, P13: 0, P14: 0, P15: 0
@@ -274,7 +274,7 @@ export const CANDIDATOS: Candidato[] = [
     nombre: "WALTER GILMER CHIRINOS PURIZAGA",
     partido: "PARTIDO POLITICO PRIN",
     foto_url: "https://mpesije.jne.gob.pe/apidocs/a2d0f631-fe47-4c41-92ba-7ed9f4095520.jpg",
-    perfil: null,
+    perfil: "Tecnócrata",
     P1: 4, P2: 5, P3: 4, P4: 5, P5: 5,
     P6: 4, P7: 0, P8: 3, P9: 3, P10: 3,
     P11: 0, P12: 4, P13: 2, P14: 2, P15: 0
@@ -284,7 +284,7 @@ export const CANDIDATOS: Candidato[] = [
     nombre: "ALFONSO CARLOS ESPA Y GARCES-ALVEAR",
     partido: "PARTIDO SICREO",
     foto_url: "https://mpesije.jne.gob.pe/apidocs/85935f77-6c46-4eab-8c7e-2494ffbcece0.jpg",
-    perfil: null,
+    perfil: "Outsider",
     P1: 1, P2: 4, P3: 5, P4: 5, P5: 4,
     P6: 1, P7: 0, P8: 1, P9: 3, P10: 5,
     P11: 0, P12: 4, P13: 2, P14: 3, P15: 0
@@ -294,7 +294,7 @@ export const CANDIDATOS: Candidato[] = [
     nombre: "CARLOS ERNESTO JAICO CARRANZA",
     partido: "PERU MODERNO",
     foto_url: "https://mpesije.jne.gob.pe/apidocs/7d91e14f-4417-4d61-89ba-3e686dafaa95.jpg",
-    perfil: null,
+    perfil: "Tecnócrata",
     P1: 2, P2: 0, P3: 4, P4: 4, P5: 5,
     P6: 5, P7: 0, P8: 2, P9: 2, P10: 4,
     P11: 0, P12: 4, P13: 0, P14: 2, P15: 0
@@ -304,7 +304,7 @@ export const CANDIDATOS: Candidato[] = [
     nombre: "JOSE LEON LUNA GALVEZ",
     partido: "PODEMOS PERU",
     foto_url: "https://mpesije.jne.gob.pe/apidocs/a669a883-bf8a-417c-9296-c14b943c3943.jpg",
-    perfil: null,
+    perfil: "Político con experiencia",
     P1: 4, P2: 4, P3: 4, P4: 5, P5: 4,
     P6: 5, P7: 0, P8: 3, P9: 2, P10: 3,
     P11: 5, P12: 4, P13: 5, P14: 4, P15: 4
@@ -314,7 +314,7 @@ export const CANDIDATOS: Candidato[] = [
     nombre: "MARIA SOLEDAD PEREZ TELLO DE RODRIGUEZ",
     partido: "PRIMERO LA GENTE - COMUNIDAD, ECOLOGIA, LIBERTAD Y PROGRESO",
     foto_url: "https://mpesije.jne.gob.pe/apidocs/073703ca-c427-44f0-94b1-a782223a5e10.jpg",
-    perfil: null,
+    perfil: "Político con experiencia",
     P1: 1, P2: 1, P3: 4, P4: 5, P5: 5,
     P6: 0, P7: 0, P8: 0, P9: 3, P10: 4,
     P11: 0, P12: 1, P13: 1, P14: 1, P15: 0
@@ -324,7 +324,7 @@ export const CANDIDATOS: Candidato[] = [
     nombre: "PAUL DAVIS JAIMES BLANCO",
     partido: "PROGRESEMOS",
     foto_url: "https://mpesije.jne.gob.pe/apidocs/929e1a63-335d-4f3a-ba26-f3c7ff136213.jpg",
-    perfil: null,
+    perfil: "Outsider",
     P1: 5, P2: 2, P3: 4, P4: 5, P5: 4,
     P6: 0, P7: 0, P8: 1, P9: 1, P10: 4,
     P11: 0, P12: 4, P13: 1, P14: 0, P15: 0
@@ -334,7 +334,7 @@ export const CANDIDATOS: Candidato[] = [
     nombre: "RAFAEL BERNARDO LOPEZ ALIAGA CAZORLA",
     partido: "RENOVACION POPULAR",
     foto_url: "https://mpesije.jne.gob.pe/apidocs/b2e00ae2-1e50-4ad3-a103-71fc7e4e8255.jpg",
-    perfil: null,
+    perfil: "Empresario",
     P1: 5, P2: 4, P3: 4, P4: 4, P5: 5,
     P6: 1, P7: 0, P8: 1, P9: 3, P10: 4,
     P11: 4, P12: 5, P13: 5, P14: 4, P15: 4
@@ -344,7 +344,7 @@ export const CANDIDATOS: Candidato[] = [
     nombre: "ANTONIO ORTIZ VILLANO",
     partido: "SALVEMOS AL PERU",
     foto_url: "https://mpesije.jne.gob.pe/apidocs/8e6b9124-2883-4143-8768-105f2ce780eb.jpg",
-    perfil: null,
+    perfil: "Outsider",
     P1: 4, P2: 0, P3: 4, P4: 5, P5: 4,
     P6: 0, P7: 4, P8: 0, P9: 2, P10: 5,
     P11: 0, P12: 0, P13: 4, P14: 0, P15: 0
@@ -354,7 +354,7 @@ export const CANDIDATOS: Candidato[] = [
     nombre: "ROSARIO DEL PILAR FERNANDEZ BAZAN",
     partido: "UN CAMINO DIFERENTE",
     foto_url: "https://mpesije.jne.gob.pe/apidocs/ac0b0a59-ead5-4ef1-8ef8-8967e322d6ca.jpg",
-    perfil: null,
+    perfil: "Outsider",
     P1: 0, P2: 5, P3: 0, P4: 5, P5: 4,
     P6: 0, P7: 0, P8: 0, P9: 2, P10: 4,
     P11: 0, P12: 4, P13: 1, P14: 0, P15: 0
@@ -364,7 +364,7 @@ export const CANDIDATOS: Candidato[] = [
     nombre: "ROBERTO ENRIQUE CHIABRA LEON",
     partido: "UNIDAD NACIONAL",
     foto_url: "https://mpesije.jne.gob.pe/apidocs/5c703ce9-ba1e-4490-90bf-61006740166f.jpg",
-    perfil: null,
+    perfil: "Político con experiencia",
     P1: 2, P2: 4, P3: 4, P4: 5, P5: 4,
     P6: 4, P7: 3, P8: 0, P9: 2, P10: 4,
     P11: 1, P12: 4, P13: 1, P14: 0, P15: 0
